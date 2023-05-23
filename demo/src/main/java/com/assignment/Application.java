f027f6f3fa6493a15c6323871eefc8252897a262
Jindddd <116243730+Jindddd@users.noreply.github.com>
2023-05-23 21:34:36 +0800

    Create the structure of GUI

--- demo/src/main/java/com/assignment/Application.java
+++ demo/src/main/java/com/assignment/Application.java
@@ -4,7 +4,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,21 +13,17 @@ public class Application extends javafx.application.Application {
     // Override the start method in the Application class
     @Override
     public void start(Stage stage) throws IOException {
-        // Root
-        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("scene1.fxml")));
-        // Scene
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("hierarchyScene.fxml")));
         Scene scene = new Scene(root);
-        // Css
         scene.getStylesheets().add(Objects.requireNonNull(getClass().getResource("style.css")).toExternalForm());
-        // Icon
         Image icon = new Image("icon.jfif");
         stage.getIcons().add(icon);
-        stage.setTitle("Gentle Reminder");
+        stage.setTitle("Wu's Kingdom Simulator");
         stage.setScene(scene);
         stage.show();
     }
 
     public static void main(String[] args) {
-        launch();
+        launch(args);
     }
 }
